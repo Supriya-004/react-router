dd884e6355a5bc3ee7961798c17784033a8feb5d
Supriya-004 <supriyawalhekar08@gmail.com>
2026-04-10 18:52:23 +0530

    code added

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./pages/Navbar";
-import Home from "./pages/home";
-import About from "./pages/about";
-import Contact from "./pages/contact";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
 
 const root = createRoot(document.getElementById("root"));
 
